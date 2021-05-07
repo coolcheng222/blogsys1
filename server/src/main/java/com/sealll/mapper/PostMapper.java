981a71111a954a3f6c75862ed0a0f18deaac6be9
tom_glb <aaa@aaa>
2021-05-07 23:19:32 +0800

    complete PostMapper

--- server/src/main/java/com/sealll/mapper/PostMapper.java
+++ server/src/main/java/com/sealll/mapper/PostMapper.java
@@ -2,6 +2,9 @@ package com.sealll.mapper;
 
 import com.sealll.application.post.bean.Post;
 import com.sealll.application.post.bean.PostExample;
+import com.sealll.application.tag.bean.Tag;
+
+import java.util.List;
 
 /**
  * @author sealll
@@ -17,4 +20,12 @@ public interface PostMapper {
     public Post getPostWithAuthorByExample(PostExample example);
     public Post getPostWithTagsByExample(PostExample example);
     public Post getPostTerminalByExample(PostExample example);
+
+    public boolean insertPost(Post post);
+    public boolean insertTags(Post post);
+
+    public boolean updateSelective(Post post);
+    public boolean deleteAllTags(String pid);
+
+    public boolean deletePostByPid(String pid);
 }
